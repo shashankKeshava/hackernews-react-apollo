20071c813ff4e5694db7202f19a13b0d9ba4d659
Shashank K <Shashank_K@intuit.com>
2019-03-08 15:59:15 +0530

    Completed till routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from "react";
-// import logo from "../logo.svg";
-// import "../styles/App.css";
+import React, {Component} from "react";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
+import Header from "./Header";
+import Login from "./Login";
+import {Switch, Route} from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return "Hi Shashank";
-  }
+    render() {
+        return (
+            <div className="center w85">
+                <Header />
+                <div className="ph3 pv1 background-gray">
+                    <Switch>
+                        <Route exact path="/" component={LinkList} />
+                        <Route exact path="/create" component={CreateLink} />
+                        <Route exact path="/login" component={Login} />
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 // import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {ApolloProvider} from "react-apollo";
+import {ApolloClient} from "apollo-client";
+import {createHttpLink} from "apollo-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
+
+import {BrowserRouter} from "react-router-dom";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
+    uri: "http://localhost:4000"
 });
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+    link: httpLink,
+    cache: new InMemoryCache()
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 // serviceWorker.unregister();
